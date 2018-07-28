8cb6a705142e846f1c3333ba53f977471452befe
ssh_jie <18700790825@163.com>
2018-07-29 00:18:25 +0800

    路由配置

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 api = Api()
 rc = Blueprint('rc', __name__)
 
-api.add_resource(Index,'/', endpoint='home')
-api.add_resource(ServerFile, '/f/<id>', endpoint='serverfile')
-api.add_resource(UploadAPI, '/upload/', endpoint='upload')
-api.add_resource(HomeworkInfoAPI, '/student_all/<id>/', endpoint='hemoinfo')
+api.add_resource(Index,'/docs/', endpoint='home')
+api.add_resource(ServerFile, '/docs/f/<id>', endpoint='serverfile')
+api.add_resource(UploadAPI, '/docs/upload/', endpoint='upload')
+api.add_resource(HomeworkInfoAPI, '/docs/student_all/<id>/', endpoint='hemoinfo')
